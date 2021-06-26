c86f78aa52a4a16c40b522ce25e599ad055c9f97
rifatahammed <reardj007@gmail.com>
2021-06-26 16:29:08 +0600

    added

---
+++ detectHand.py
@@ -0,0 +1,29 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv2.imshow("RIFFFFFs Hand Detector", img)
+
+    key = cv2.waitKey(1)
+
+    # segment for press q or Q to quit the app
+    if key == 81 or key == 113:
+        break
+
+
+print('THIS IS YOUR HAND')
